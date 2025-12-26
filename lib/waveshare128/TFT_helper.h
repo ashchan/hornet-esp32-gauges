0ba7a579369c1e7442c84a7ecb2d04e73fc8df7b
Jianming Chen <ashchan@gmail.com>
2025-12-26 16:11:46 +0900

    Make 1.28 display less bright by default

--- lib/waveshare128/TFT_helper.h
+++ lib/waveshare128/TFT_helper.h
@@ -3,11 +3,11 @@
 #include <Arduino.h>
 #include <cstdint>
 
-#define DEFAULT_TFT_BRIGHTNESS 40
+#define DEFAULT_TFT_BRIGHTNESS 20
 
 // value: 0-65536, map to DEFAULT_TFT_BRIGHTNESS - 200 (display content even if input is 0)
 static void setBrightness(uint16_t value) {
-  static uint16_t oldValue = DEFAULT_TFT_BRIGHTNESS;
+  static uint16_t oldValue = 0;
   uint16_t newValue = map(value, 0, 65535, DEFAULT_TFT_BRIGHTNESS, 200);
   if (oldValue != newValue) {
     oldValue = newValue;
