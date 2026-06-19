b9c351c0a1a73847e4ae9d90f9860884644afcc7
Jianming "Ash" Chen <ashchan@gmail.com>
2026-06-19 18:02:22 +0900

    Add instrument light sync to the compass

--- src/standby_compass/main.cpp
+++ src/standby_compass/main.cpp
@@ -10,10 +10,22 @@
 static volatile uint16_t brightness = 0;
 static volatile uint16_t headingRaw = 0; // 0-65535
 static volatile bool dirty = true;
+static volatile bool brightnessDirty = true;
 static uint32_t lastFrameUs = 0;
 static const uint32_t FRAME_INTERVAL_US = 33333;  // ~30 FPS
 static const bool kTestLoopEnabled = false;
 static const uint32_t kTestHeadingRawPerSecond = 5461; // ~30 degrees/second
+static const uint8_t kMinimumBacklight = 10;
+static const uint8_t kMaximumBacklight = 80;
+
+static void setCompassBrightness(uint16_t value) {
+  static uint8_t oldValue = 0xff;
+  const uint8_t newValue = (uint8_t)map(value, 0, 65535, kMinimumBacklight, kMaximumBacklight);
+  if (oldValue != newValue) {
+    oldValue = newValue;
+    Set_Backlight(newValue);
+  }
+}
 
 static void initEspNowClient() {
   WiFi.mode(WIFI_STA);
@@ -41,6 +53,9 @@ static void initEspNowClient() {
       if (message.name == ValueName::MagneticHeading) {
         headingRaw = message.value;
         dirty = true;
+      } else if (message.name == ValueName::InstrumentLighting) {
+        brightness = message.value;
+        brightnessDirty = true;
       }
       break;
     default:
@@ -53,7 +68,9 @@ void setup() {
   Serial.begin(115200);
 
   delay(1000);
+  LCD_Backlight = kMinimumBacklight;
   LCD_Init();
+  setCompassBrightness(brightness);
   Renderer_Init();
   lastFrameUs = micros();
 
@@ -65,6 +82,15 @@ void setup() {
 void loop() {
   const uint32_t now = micros();
   const bool frameDue = (now - lastFrameUs) >= FRAME_INTERVAL_US;
+
+  if (brightnessDirty) {
+    noInterrupts();
+    const uint16_t b = brightness;
+    brightnessDirty = false;
+    interrupts();
+
+    setCompassBrightness(b);
+  }
 
   if (kTestLoopEnabled && frameDue) {
     lastFrameUs += FRAME_INTERVAL_US;
